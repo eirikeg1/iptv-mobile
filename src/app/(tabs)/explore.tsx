beebb2085d58db792db758066cadc6c6bedde165
eirikeg1 <e.eggset@gmail.com>
2025-10-13 22:28:21 +0200

    Fix errors, and add football splash and parallax background

--- src/app/(tabs)/explore.tsx
+++ src/app/(tabs)/explore.tsx
@@ -1,12 +1,11 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet } from 'react-native';
 
-import { Collapsible } from '@/components/ui/collapsible';
 import { ExternalLink } from '@/components/external-link';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
-import { IconSymbol } from '@/components/ui/icon-symbol';
+import { Collapsible } from '@/components/ui/collapsible';
 import { Fonts } from '@/lib/theme';
 
 export default function TabTwoScreen() {
@@ -14,11 +13,15 @@ export default function TabTwoScreen() {
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
       headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
+        // <IconSymbol
+        //   size={310}
+        //   color="#808080"
+        //   name="chevron.left.forwardslash.chevron.right"
+        //   style={styles.headerImage}
+        // />
+        <Image
+          source={require('../../../assets/images/football-scene-horizontal.png')}
+          style={styles.footballImage}
         />
       }>
       <ThemedView style={styles.titleContainer}>
@@ -108,5 +111,12 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
     gap: 8,
+  },
+  footballImage: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
   },
 });
